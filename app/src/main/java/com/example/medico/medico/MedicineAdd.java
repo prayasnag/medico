86ed653083e7d921ac7d2e8c37900c5d104ba478
Prayas Nag <prayas.nag123@gmail.com>
2019-04-23 16:25:40 +0530

    commit 23/04 Added date picker and time picker Added few string values but not used

--- app/src/main/java/com/example/medico/medico/MedicineAdd.java
+++ app/src/main/java/com/example/medico/medico/MedicineAdd.java
@@ -1,5 +1,9 @@
 package com.example.medico.medico;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,12 +11,16 @@ import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
+import java.time.Month;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -22,6 +30,16 @@ public class MedicineAdd extends AppCompatActivity implements AdapterView.OnItem
     private static final String TAG = "MedicineAdd";
     DatabaseHelper mDatabaseHelper;
     private EditText editText;
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private TextView mDisplayTime1;
+    private TextView mDisplayTime2;
+    private TextView mDisplayTime3;
+    private TextView mDisplayTime4;
+    private DatePickerDialog.OnDateSetListener mTimeSetListener1;
+    private DatePickerDialog.OnDateSetListener mTimeSetListener2;
+    private DatePickerDialog.OnDateSetListener mTimeSetListener3;
+    private DatePickerDialog.OnDateSetListener mTimeSetListener4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +47,11 @@ public class MedicineAdd extends AppCompatActivity implements AdapterView.OnItem
         setContentView(R.layout.activity_medicine_add);
 
         //Variables Declaration
-        editText =(EditText) findViewById(R.id.editMedName);
+        editText = findViewById(R.id.editMedName);
         mDatabaseHelper = new DatabaseHelper(this);
 
-
+        //for hiding soft keypad
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-
-        //update for git
 
         //Start of code for spinner for reminder
 
@@ -47,6 +63,133 @@ public class MedicineAdd extends AppCompatActivity implements AdapterView.OnItem
 
         String spinnerText = spinner.getSelectedItem().toString();
 
+        //code for time selector
+            //first
+        mDisplayTime1 = (TextView)findViewById(R.id.timeOne);
+        mDisplayTime1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int hour = cal.get(Calendar.HOUR);
+                int min = cal.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog;
+                timePickerDialog = new TimePickerDialog(MedicineAdd.this, R.style.TimePickerTheme, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                        String time = String.format("%02d:%02d", hourOfDay, minute);
+                        mDisplayTime1.setText(time);
+                    }
+                },hour,min,true);
+                timePickerDialog.show();
+            }
+        });
+        //two
+        mDisplayTime2 = (TextView)findViewById(R.id.timeTwo);
+        mDisplayTime2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int hour = cal.get(Calendar.HOUR);
+                int min = cal.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog;
+                timePickerDialog = new TimePickerDialog(MedicineAdd.this,R.style.TimePickerTheme, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                        String time = String.format("%02d:%02d", hourOfDay, minute);
+                        mDisplayTime2.setText(time);
+                    }
+                },hour,min,true);
+                timePickerDialog.show();
+            }
+        });
+
+        //three
+        mDisplayTime3 = (TextView)findViewById(R.id.timeThree);
+        mDisplayTime3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int hour = cal.get(Calendar.HOUR);
+                int min = cal.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog;
+                timePickerDialog = new TimePickerDialog(MedicineAdd.this,R.style.TimePickerTheme, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                        String time = String.format("%02d:%02d", hourOfDay, minute);
+                        mDisplayTime3.setText(time);
+                    }
+                },hour,min,true);
+                timePickerDialog.show();
+            }
+        });
+
+        //four
+        mDisplayTime4 = (TextView)findViewById(R.id.timeFour);
+        mDisplayTime4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int hour = cal.get(Calendar.HOUR);
+                int min = cal.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog;
+                timePickerDialog = new TimePickerDialog(MedicineAdd.this,R.style.TimePickerTheme, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                        String time = String.format("%02d:%02d", hourOfDay, minute);
+                        mDisplayTime4.setText(time);
+                    }
+                },hour,min,true);
+                timePickerDialog.show();
+            }
+        });
+        //code for date selector
+        mDisplayDate = (TextView)findViewById(R.id.StartDate);
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(cal.YEAR);
+                int month = cal.get(cal.MONTH);
+                int day = cal.get(cal.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(MedicineAdd.this,android.R.style.Theme_Holo_Light_Dialog_MinWidth,mDateSetListener,year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                String monthText;
+                month = month+1;
+                if(month == 1)
+                    monthText = "Jan";
+                else if(month == 2 )
+                    monthText = "Feb";
+                else if(month == 3 )
+                    monthText = "Mar";
+                else if(month == 4 )
+                    monthText = "Apr";
+                else if(month == 5 )
+                    monthText = "May";
+                else if(month == 6 )
+                    monthText = "Jun";
+                else if(month == 7 )
+                    monthText = "Jul";
+                else if(month == 8 )
+                    monthText = "Aug";
+                else if(month == 9 )
+                    monthText = "Sept";
+                else if(month == 10 )
+                    monthText = "Oct";
+                else if(month == 11 )
+                    monthText = "Nov";
+                else
+                    monthText = "Dec";
+                String date = monthText +" "+day+", "+year;
+                mDisplayDate.setText(date);
+            }
+        };
 
         //Data Collection Method
         try {
@@ -100,25 +243,25 @@ public class MedicineAdd extends AppCompatActivity implements AdapterView.OnItem
         TextView timePicker4 = findViewById(R.id.timeFour);
         timePicker4.setVisibility(View.GONE);
         String spinnerText = parent.getItemAtPosition(position).toString();
-        if(spinnerText.equals("once a day"))
-            timePicker1.setVisibility(View.VISIBLE);
-        else if(spinnerText.equals("twice a day"))
-        {
-            timePicker1.setVisibility(View.VISIBLE);
-            timePicker2.setVisibility(View.VISIBLE);
-        }
-        else if(spinnerText.equals("thrice a day"))
-        {
-            timePicker1.setVisibility(View.VISIBLE);
-            timePicker2.setVisibility(View.VISIBLE);
-            timePicker3.setVisibility(View.VISIBLE);
-        }
-        else
-        {
-            timePicker1.setVisibility(View.VISIBLE);
-            timePicker2.setVisibility(View.VISIBLE);
-            timePicker3.setVisibility(View.VISIBLE);
-            timePicker4.setVisibility(View.VISIBLE);
+        switch (spinnerText) {
+            case "once a day":
+                timePicker1.setVisibility(View.VISIBLE);
+                break;
+            case "twice a day":
+                timePicker1.setVisibility(View.VISIBLE);
+                timePicker2.setVisibility(View.VISIBLE);
+                break;
+            case "thrice a day":
+                timePicker1.setVisibility(View.VISIBLE);
+                timePicker2.setVisibility(View.VISIBLE);
+                timePicker3.setVisibility(View.VISIBLE);
+                break;
+            default:
+                timePicker1.setVisibility(View.VISIBLE);
+                timePicker2.setVisibility(View.VISIBLE);
+                timePicker3.setVisibility(View.VISIBLE);
+                timePicker4.setVisibility(View.VISIBLE);
+                break;
         }
 
     }
